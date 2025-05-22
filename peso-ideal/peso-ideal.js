e1f6c72ec3443ad606b4b213a520735dc3f4be90
Gustavo Brandão <gustavo1731@hotmail.com>
2025-05-22 18:26:43 -0300

    atualizar

---
+++ peso-ideal/peso-ideal.js
@@ -0,0 +1,24 @@
+const form = document.querySelector("form");
+const resp = document.querySelector("#resp");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const nome = form.nome.value;
+    const masculino = form.masculino.checked;
+    const altura = form.altura.value;
+
+    let peso;
+
+    if (masculino) {    //if masculino == true
+        peso = 22 * Math.pow(altura, 2);    //eleva ao quadrado.
+    } else {
+        peso = 21 * Math.pow(altura, 2);
+    }
+
+    resp.innerText = `${nome}: Seu peso ideal é ${peso.toFixed(2)} kg`
+});
+
+form.addEventListener("reset", (event) => {
+    resp.innerText = "";       //limpa o elemento h3.
+})
