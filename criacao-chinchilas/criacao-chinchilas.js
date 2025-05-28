c52865001f8e8843051efa27819ff4397b5d8ae0
Gustavo Brandão <gustavo1731@hotmail.com>
2025-05-28 12:31:16 -0300

    atualização

---
+++ criacao-chinchilas/criacao-chinchilas.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("form");
+const resposta = document.querySelector("pre");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let chinchilas = Number(form.chinchilas.value);
+    let anos = Number(form.anos.value);
+
+    let repeticao = "" 
+
+    for (i = 0; i < anos; i++) {
+        
+        repeticao += `${i}º Ano: ${chinchilas} Chinchilas \n`
+        chinchilas = chinchilas * 3
+    }
+
+    resposta.innerText = repeticao;
+});
